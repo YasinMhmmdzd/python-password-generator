453cdfea23789a92b5dfa23cd858221fcacf533e
Yasin Mohammadzade <103330832+YasinMhmmdzd@users.noreply.github.com>
2023-01-06 00:03:59 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import random
 from colorama import Fore
 while True:
     print(Fore.BLUE + "welcome")
-    password_range = int(input("Enter your password range >>> "))
+    password_range = int(input("Enter your password range (type 0 to close the programm) >>> "))
     allow_chars = ('aqzswxdecrfvgtbhynjumki,lo.p;[1234567890-=!@#$%^&*()_+/*-`~')
     if password_range >= 10 :
         for item in range(password_range):
             item = random.choice(allow_chars)
             print(Fore.GREEN + item , end='')
+    elif password_range == 0:
+        break
     else:
         print(Fore.YELLOW + 'range should up to 10')
