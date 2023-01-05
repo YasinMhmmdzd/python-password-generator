5d06a9c8fedee751c3398a71f68a54277e622539
Yasin Mohammadzade <103330832+YasinMhmmdzd@users.noreply.github.com>
2023-01-05 23:50:27 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ allow_chars = ('aqzswxdecrfvgtbhynjumki,lo.p;[1234567890-=!@#$%^&*()_+/*-`~')
 if int(password_range) > 10 :
     for item in range(password_range):
         item = random.choice(allow_chars)
-        print(i , end='')
+        print(item , end='')
 else:
     print('range should up to 10')
