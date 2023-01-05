568c66538e2cada2c8a17843f6363cac8fbe6c88
Yasin Mohammadzade <103330832+YasinMhmmdzd@users.noreply.github.com>
2023-01-06 00:39:36 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import random
 from colorama import Fore
+import pyperclip
 print(Fore.BLUE + "welcome")
 while True:
-    password_range = int(input(Fore.LIGHTBLUE_EX + "Enter your password range (type 0 to close the programm) >>> "))
+    chars = []
+    password_range = int(input(Fore.LIGHTBLUE_EX + "Enter your password range " + Fore.LIGHTYELLOW_EX + "(type 0 to close the programm) >>> "))
     allow_chars = ('aqzswxdecrfvgtbhynjumki,lo.p;[1234567890-=!@#$%^&*()_+/*-`~')
     if password_range >= 10 :
         for item in range(password_range):
             item = random.choice(allow_chars)
-            print(Fore.GREEN + item , end='')
+            chars.append(item)
+        new_password = ''.join(chars)
+        print(Fore.GREEN + new_password)
+        pyperclip.copy(new_password)
+        print("password copid to clipboard !!!")
         print("")
     elif password_range == 0:
         break
