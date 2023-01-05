21989e778b6a30b6eacc49d0d05740552608fb30
Yasin Mohammadzade <103330832+YasinMhmmdzd@users.noreply.github.com>
2023-01-05 23:25:54 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import random
+print("welcome")
+password_range = input("Enter your password range")
+allow_chars = ('aqzswxdecrfvgtbhynjumki,lo.p;[1234567890-=!@#$%^&*()_+/*-`~')
+if password_range >= 10 :
+    for item in range(password_range):
+        item = random.choice(allow_chars)
+        print(i , end='')
+else:
+    print('range should up to 10')
