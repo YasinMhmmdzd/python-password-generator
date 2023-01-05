393e87379566a65184dd361edc53a88564e64611
Yasin Mohammadzade <103330832+YasinMhmmdzd@users.noreply.github.com>
2023-01-05 23:58:20 +0330

    set a while for unlimiting program

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import random
 from colorama import Fore
-print(Fore.BLUE + "welcome")
-password_range = int(input("Enter your password range"))
-allow_chars = ('aqzswxdecrfvgtbhynjumki,lo.p;[1234567890-=!@#$%^&*()_+/*-`~')
-if password_range >= 10 :
-    for item in range(password_range):
-        item = random.choice(allow_chars)
-        print(Fore.GREEN + item , end='')
-else:
-    print('range should up to 10')
+while True:
+    print(Fore.BLUE + "welcome")
+    password_range = int(input("Enter your password range"))
+    allow_chars = ('aqzswxdecrfvgtbhynjumki,lo.p;[1234567890-=!@#$%^&*()_+/*-`~')
+    if password_range >= 10 :
+        for item in range(password_range):
+            item = random.choice(allow_chars)
+            print(Fore.GREEN + item , end='')
+    else:
+        print('range should up to 10')
